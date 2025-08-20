33bca6ba6891143998ef102b522d74d5567594ad
halaluddin2000 <helaluddin7001@gmail.com>
2025-08-20 07:04:07 +0600

    hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,26 @@
 import "./hero.css";
+import dark_arrow from "../../assets/arrow.jpg";
+import { motion } from "motion/react";
 
 function Hero() {
   return (
     <div className="hero container">
-      <div className="hero-text">
-        <h2>We Ensure better education for a better world</h2>
-        <h5>
+      <motion.div
+        className="hero-text "
+        initial={{ opacity: 0, y: 80 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ type: "spring", stiffness: 80, delay: 0.5 }}
+      >
+        <h1>We Ensure better education for a better world </h1>
+        <p>
           Our cutting-edge curriculum is designed to empower students with the
           knowledge, skills, and experiences needed to excel in the dynamic
           field of education
-        </h5>
-        <button className="btn">Explore more</button>
-      </div>
+        </p>
+        <button className="btn">
+          Explore more <img src={dark_arrow} alt="" />
+        </button>
+      </motion.div>
     </div>
   );
 }
